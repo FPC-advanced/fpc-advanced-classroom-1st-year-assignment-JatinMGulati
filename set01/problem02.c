7d4b1d2d242126a55cf60b6ad7f76da8d5198b19
Jatin Gulati <jmgulati007@gmail.com>
2023-10-17 12:10:32 +0530

    committed 10/17/2023, 12:10:31 PM

--- set01/problem02.c
+++ set01/problem02.c
@@ -1,2 +1,13 @@
 //2. Write a C program to add two numbers.
-
+#include <stdio.h>
+int main()
+{
+    int a,b,sum=0;
+    printf("Enter the first number:\n");
+    scanf("%d",b);
+    printf("Enter the Second number:\n");
+    scanf("%d",a);
+    sum=a+b;
+    printf("The sum of %d and %d is: %d\n",a,b,sum);
+    return 0;
+}
---
+++ set01/problem03.c
@@ -0,0 +1,30 @@
+//Write a C program to add two numbers using **pass by value**
+#include <stdio.h>
+int input();
+int add(int a, int b);
+void output(int a, int b, int sum);
+
+int main()
+{
+    int a,b,sum=0;
+    a=input();
+    b=input();
+    sum=add(a,b);
+    output(a,b,sum);
+    return 0;
+}
+int input()
+{
+    int x;
+    printf("Enter the number:");
+    scanf("%d",&x);
+    return x;
+}
+int add(int a, int b)
+{
+    return a+b;
+}
+void output(int a, int b, int sum)
+{
+    printf("The sum of %d and %d is: %d",a,b,sum);
+}
