002546059eae5b525301ae05e5ca676160f787c7
Jatin Gulati <jmgulati007@gmail.com>
2023-10-30 04:12:53 +0530

    committed 10/30/2023, 4:12:51 AM

--- set02/problem01.c
+++ set02/problem01.c
@@ -21,6 +21,7 @@ void input(float *base, float *height)
 void find_area(float base, float height, float *area)
 {
     area=0.5*base*height;
+}
 void output(float base, float height, float area)
 {
     printf("The area of the triangle with base %f and %f is: %f",base,height,area);
