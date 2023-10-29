3bbb8670df70b437cd7420be59346fdc01e95557
Jatin Gulati <jmgulati007@gmail.com>
2023-10-30 03:58:22 +0530

    committed 10/30/2023, 3:58:21 AM

---
+++ set02/problem01.c
@@ -0,0 +1,16 @@
+//Write a program to find the area of a triangle.
+#include <stdio.h>
+void input(float *base, float *height);
+void find_area(float base , float height, float *area);
+void output(float base, float height, float area);
+
+int main()
+{
+    int base,height,area;
+    input(&base,&height);
+
+}
+void input(float *base, float *height)
+{
+    printf("enter the va")
+}
