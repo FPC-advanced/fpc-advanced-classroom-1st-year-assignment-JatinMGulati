0f6da74d053a8dafffc4fc5b2fb5e0d273f817a8
Jatin Gulati <jmgulati007@gmail.com>
2023-10-31 06:23:06 +0530

    committed 10/31/2023, 6:23:05 AM

--- set02/problem02.c
+++ set02/problem02.c
@@ -7,12 +7,34 @@ void output(int a, int b, int c, int isscalene);
 int main()
 {
     int a,b,c,isscalene;
-    
+    a=input_side();
+    b=input_side();
+    c=input_side();
+    isscalene=check_scalene(a,b,c);
+    output(a,b,c,isscalene);
 }
 int input_side()
 {
     int x;
     printf("Enter the value:");
-    scanf()
+    scanf("%f",x);
+    return x;
 }
-
+int check_scalene(int a,int b,int c)
+{
+    if(a!=b && b!=c && a!=c)
+    {
+       return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+void output(int a, int b, int c, int isscalene)
+{
+    if(isscalene==1)
+    {
+        printf("")
+    }
+}
---
+++ set02/problem03.c
@@ -0,0 +1,41 @@
+//Write a program find whether a number is a composite number
+//>A Composite number has more than 2 factors.
+#include <stdio.h>
+int input_number();
+int is_composite(int n);
+void output(int n, int result);
+
+int main()
+{
+    int n,result;
+    n=input_number();
+    result=is_composite(n);
+    output(n,result);
+}
+int input_number()
+{
+    int x;
+    printf('Enter the number:');
+    scanf('%d',&x);
+    return x;
+}
+int is_composite(int n)
+{
+    for(int i=1;i<=n,i++)
+    {
+       if(n%i==0)
+       {
+        return 1;
+       }
+       return 0;
+    }
+}
+void output(int n, int result)
+{
+    if(result==1){
+        printf("%d is a composite number",n);
+    }
+    else{
+        printf('%d is not a composite number',n);
+    }
+}
