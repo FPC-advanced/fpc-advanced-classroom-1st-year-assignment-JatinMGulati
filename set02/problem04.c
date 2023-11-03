ac363be33ff92ad451969034718bdb393444142c
Jatin Gulati <jmgulati007@gmail.com>
2023-11-03 23:14:27 +0530

    committed 11/3/2023, 11:14:26 PM

--- set02/problem04.c
+++ set02/problem04.c
@@ -42,15 +42,7 @@ int sum_composite_numbers(int n, int a[n])
         }
     }
     return 0;
-    {
-    int sum = 0;
-    for (int i = 0; i < n; i++) {
-        if (is_composite(a[i])) {
-            sum += a[i];
-        }
-    }
-    return sum;
-}
+    
 }
 void output(int sum)
 {
