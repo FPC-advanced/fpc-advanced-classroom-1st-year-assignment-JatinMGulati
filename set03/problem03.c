4ab2467c9c7721f41ad6746314d161792bfa37fb
Jatin Gulati <jmgulati007@gmail.com>
2023-11-08 23:06:02 +0530

    committed 11/8/2023, 11:06:01 PM

---
+++ set03/problem03.c
@@ -0,0 +1,29 @@
+//Write a program to check if the given number is prime
+
+#include <stdio.h>
+int input_number();
+int is_prime(int n);
+void output(int n, int result);
+
+int main()
+{
+    int n,result;
+    n=
+}
+int input_number()
+{
+    int x;
+    printf("Enter the value:");
+    scanf("%d",x);
+    return x;
+}
+int is_prime(int n)
+{
+    for(int i=1;i<=n,i++)
+    {
+        if(n%i==0)
+        {
+            
+        }
+    }
+}
