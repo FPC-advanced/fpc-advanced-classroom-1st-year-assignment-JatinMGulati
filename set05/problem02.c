bd4d1f5a5be4fb18cad2708aa3bb744fefe80cff
Jatin Gulati <jmgulati007@gmail.com>
2023-11-15 14:52:39 +0530

    committed 11/15/2023, 2:52:38 PM

--- set05/problem02.c
+++ set05/problem02.c
@@ -3,3 +3,11 @@
 > `weight = pi * stomach_radius^3 * sqrt(height * length)`*/
 #include <stdio.h>
 #include <math.h>
+void input_camel_details(float *radius, float *height, float *length);
+float find_weight(float radius, float height, float length);
+void output(float radius, float height, float length, float weight);
+
+int main()
+{
+    
+}
