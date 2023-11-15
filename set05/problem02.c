1625a50043b63bde0d185fb454fbd1da2be2f146
Jatin Gulati <jmgulati007@gmail.com>
2023-11-15 14:51:09 +0530

    committed 11/15/2023, 2:51:08 PM

--- set05/problem02.c
+++ set05/problem02.c
@@ -1,3 +1,3 @@
-// Write a program to find the weight of a camel, given height, length and stomach radius using four functions.
+/* Write a program to find the weight of a camel, given height, length and stomach radius using four functions.
 
-> `weight = pi * stomach_radius^3 * sqrt(height * length)`
+> `weight = pi * stomach_radius^3 * sqrt(height * length)`*/
