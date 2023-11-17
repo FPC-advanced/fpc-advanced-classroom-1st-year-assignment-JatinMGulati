330fdbfad26c4c821da47a6b5b8f34822ea4616b
Jatin Gulati <jmgulati007@gmail.com>
2023-11-17 07:01:57 +0530

    committed 11/17/2023, 7:01:56 AM

--- set01/problem09.c
+++ set01/problem09.c
@@ -18,15 +18,16 @@ float input()
     scanf("%f",&n);
     return n;
 }
-float square_root(float n)
+float square_root(float n) 
 {
-    float x = n/2;
-    float epsilon=0.000001;
-    while((x*x-n)> epsilon)
-    {
-     x=(x+n/x)/2;
+    float x = n / 2;
+    float epsilon = 0.000001;
+
+    while ((x - n / x) > epsilon) {
+        x = 0.5 * (x + n / x);
     }
-    return x ;
+
+    return x;
 }
 void output(float n , float sqrroot)
 {
