54559b7973f7fd7ce22ef4bde24a30989e379f2e
Jatin Gulati <jmgulati007@gmail.com>
2023-12-05 12:09:51 +0530

    committed 12/5/2023, 12:09:50 PM

--- set03/problem04.c
+++ set03/problem04.c
@@ -10,3 +10,29 @@ int main(){
 
     return 0;
 }
+int input(){
+    int n;
+    printf("Enter the nth term: ");
+    scanf("%d", &n);
+    return n;
+}
+
+int find_fibo(int n){
+    if(n <= 1)
+        return n;
+
+    int prev = 0;
+    int curr = 1;
+
+    for(int i = 2; i <= n; i++){
+        int next = prev + curr;
+        prev = curr;
+        curr = next;
+    }
+
+    return curr;
+}
+
+void output(int n, int fibo){
+    printf("The %dth term of the Fibonacci sequence is: %d\n", n, fibo);
+}
