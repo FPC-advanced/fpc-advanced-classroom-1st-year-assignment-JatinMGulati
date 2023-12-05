787594ebde62fe6d6372c587d343b60041269848
Jatin Gulati <jmgulati007@gmail.com>
2023-12-05 10:36:10 +0530

    committed 12/5/2023, 10:36:08 AM

--- set02/problem08.c
+++ set02/problem08.c
@@ -53,7 +53,7 @@ void find_n_areas(int n, Triangle t[n])
 Triangle find_smallest_triangle(int n, Triangle t[n]) 
 {
 	Triangle smallest = t[0];
-	for (int i = 1; i < n; i++) {
+	for (int i = 1;i<n; i++) {
 		if (t[i].area < smallest.area) {
 			smallest = t[i];
 		}
