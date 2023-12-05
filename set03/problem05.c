92618602da0278276da5268b74f33c45d8b6ae65
Jatin Gulati <jmgulati007@gmail.com>
2023-12-05 12:20:22 +0530

    committed 12/5/2023, 12:20:20 PM

--- set03/problem05.c
+++ set03/problem05.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 int input_array_size();
-void init_array(int n, int a[n];
+void init_array(int n, int a[n]);
 void erotosthenes_sieve(int n, int a[n]);
 void output(int n, int a[n]);
 
-int main(){
-    int n = input_array_size();
-    int a[n];
+int main()
+{
+    int n,a[n];
+    n = input_array_size();
     init_array(n, a);
     erotosthenes_sieve(n, a);
     output(n, a);
-
     return 0;
 }
 
